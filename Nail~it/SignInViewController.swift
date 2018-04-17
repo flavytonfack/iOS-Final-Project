74c130c910d3e07370a983811953749b8d0648b1
flavytonfack <flavytonfack@gmail.com>
2018-04-17 13:54:01 -0700

    First Commit

---
+++ Nail~it/SignInViewController.swift
@@ -0,0 +1,10 @@
+//
+//  SignInViewController.swift
+//  Nail~it
+//
+//  Created by Flavy Tonfack on 4/17/18.
+//  Copyright © 2018 FLAVY TONFACK. All rights reserved.
+//
+
+import Foundation
+import UIKit
